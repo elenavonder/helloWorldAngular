df5b30283329f02cb19235dfa86025044bf49d2a
Elena Vonder Meulen <evonder13@gmail.com>
2020-11-09 09:25:53 -0500

    changed settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+//this is an object
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//objects we make will be classes
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'Bootcamp in Angular';
+
+  //property
+  txt:string = "initial value";
+
+  clicked(){
+    this.title = "Changed the title message";
+  }
 }
